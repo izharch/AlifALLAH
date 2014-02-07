f3a8ad1e56d1da96d8b18fb2d64f9f3b987e3e80
Muhammad Izhar <g.chohdry.g@gmail.com>
2014-02-08 02:58:24 +0500

    Homepage HTML completed

--- AA-HTML/js/custom.js
+++ AA-HTML/js/custom.js
@@ -1,7 +1,11 @@
 $(function() {
+    //jQuery UI
     $( ".jquery-ui-tabs" ).tabs();
     $( ".jquery-ui-menu" ).menu();
-    
+
+    //Bootsrtap
+    $('.bootstrap-carousel').carousel();
+
     //Set sidebar height
     setSidebarMinHeight();
     $(window).resize(setSidebarMinHeight);
