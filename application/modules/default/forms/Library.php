3f1c8c5845e0c19e30d1c18e6cc840ac14b8a660
Muhammad Izhar <g.chohdry.g@gmail.com>
2014-03-22 02:52:55 +0500

    Revert "Iibrarycontroller added."
    This reverts commit 126bbaceeeed0f2c967a63663074000482e8ae9a.
    
    Conflicts:
    	.zfproject.xml

--- application/modules/default/forms/Library.php
+++ application/modules/default/forms/Library.php
@@ -12,20 +12,20 @@ class Default_Form_Library extends Zend_Form
                 ->setRequired(TRUE)
                 ->addFilter('stringtrim')
                 ->addFilter('striptags')
-                ->setDecorators(array('ViewHelpers', 'Errors'));
+                ->setDecorators(array('ViewHelper', 'Errors'));
 
         $type = new Zend_Form_Element_Text('type');
         $type->setLabel('Catagory')
                 ->setRequired(true)
                 ->addFilter('stringtrim')
                 ->addFilter('striptags')
-                ->setDecorators(array('ViewHelpers', 'Errorrs'));
+                ->setDecorators(array('ViewHelper', 'Errors'));
 
         $author = new Zend_Form_Element_Text('author');
-        $artist->setLabel('author')
+        $author->setLabel('Author')
                 ->setRequired(TRUE)
                 ->addFilter('stringtrim')
-                ->addFilter(striptags)
+                ->addFilter('striptags')
                 ->setDecorators(array('ViewHelper', 'Errors'));
 
         $shareStatus = new Zend_Form_Element_Select('share_status');
@@ -35,7 +35,7 @@ class Default_Form_Library extends Zend_Form
         'shared' => 'Shared',
         'not_shared' => 'Not Shared',
         ))
-                ->setDecorators(array('ViewHelpers', 'Errors'));
+                ->setDecorators(array('ViewHelper', 'Errors'));
         
         $file = new Zend_Form_Element_File('file');
        $file->setLabel('Library File')
@@ -44,7 +44,7 @@ class Default_Form_Library extends Zend_Form
                ->addValidator('Count', FALSE, 1)
                ->addValidator('Size', FALSE, 100000000)//100MB
                ->addValidator('Extension', FALSE, array('pdf'))
-               ->setDestination('upload/library')
+               ->setDestination('uploads/library')
                ->setAttrib('accept', '.pdf')
                ->setDecorators(array('File','Errors'));
        
@@ -54,12 +54,12 @@ class Default_Form_Library extends Zend_Form
                ->setRequired(FALSE)
                ->addValidator('Count', FALSE, 1)
                ->addValidator('Size', FALSE, 1000000)//1MB
-               ->addValidator('Extention',FALSE,array('pdf'))
-               ->setDestination('uploads/thumbanils')
-               ->setAttrib('accept', '.pdf')
+               ->addValidator('Extension',FALSE,array('jpg', 'jpeg', 'png', 'gif'))
+               ->setDestination('uploads/thumbnails')
+               ->setAttrib('accept', '.jpg,.jpeg,.png,.gif')
                ->setDecorators(array('File','Errors'));
        
-       $this->addElement(array($name, $type, $author, $shareStatus, $file, $thumbnail));
+       $this->addElements(array($name, $type, $author, $shareStatus, $file, $thumbnail));
               
     }
     
